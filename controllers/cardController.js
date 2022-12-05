dba459514716d442da5a382035a8fae8022d9d76
Endobyte <endobyte@gmail.com>
2022-12-05 11:39:23 -0800

    Start work on second card

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,5 +1,5 @@
 module.exports.viewCards = function(req, res) {
-    const card = {
+    const cards = [{
         id: 1,
         type: 'electric',
         name: 'Pikachu',
@@ -10,7 +10,8 @@ module.exports.viewCards = function(req, res) {
         moveNameOne: 'Gnaw',
         moveCostOneNum: 10,
         moveCostTwo: 2,
-        mostNameTwo: 'Lightning Bolt',
+        moveNameTwo: 'Lightning Bolt',
+        moveCostTwoNum: 50,
         weaknessOne: '/images/ground.png',
         weaknessTwo: '',
         weaknessThree: '',
@@ -18,6 +19,26 @@ module.exports.viewCards = function(req, res) {
         resistanceTwo: '/images/steel.png',
         resistanceThree: '/images/electric.png',
         retreatCost: 2
-    }
-    res.render('index', card);
+    }, {
+        id: 2,
+        type: 'electric',
+        name: 'Pikachu',
+        hp: 70,
+        typeIcon: '/images/electric.png',
+        picture: 'https://vignette.wikia.nocookie.net/wii/images/7/77/Pikachu_SSB4.jpg/revision/latest?cb=20140210000421',
+        moveCostOne: 1,
+        moveNameOne: 'Quick Attack',
+        moveCostOneNum: 10,
+        moveCostTwo: 2,
+        moveNameTwo: 'Electro Ball',
+        moveCostTwoNum: 60,
+        weaknessOne: '/images/ground.png',
+        weaknessTwo: '',
+        weaknessThree: '',
+        resistanceOne: '/images/flying.png',
+        resistanceTwo: '/images/steel.png',
+        resistanceThree: '/images/electric.png',
+        retreatCost: 1
+    }];
+    res.render('index', {cards});
 }
